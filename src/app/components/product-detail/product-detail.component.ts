e479fe71376094a4bcbad0b779586884587a86d2
yeyo671 <leotran671@gmail.com>
2024-12-20 11:42:28 +0100

    add others products posted by the user on product details page

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../services/product.service';
-import { Product } from '../../models/product.model';
-import { CommonModule } from '@angular/common';
 import { ProductWithUserEmail } from '../../models/productWithUserEmail.model';
+import { CommonModule } from '@angular/common';
+import { TruncatePipe } from '../../pipes/truncate.pipe';
+import { HeaderComponent } from '../header/header.component';
+
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule, TruncatePipe, HeaderComponent]
 })
 export class ProductDetailComponent implements OnInit {
-
   product: ProductWithUserEmail | undefined;
+  otherProducts: ProductWithUserEmail[] = [];
 
   constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) {}
 
   async ngOnInit() {
+    await this.loadProductDetails();
+  }
+
+  async loadProductDetails() {
     const productId = this.route.snapshot.paramMap.get('id');
     if (productId) {
       this.product = await this.productService.getProductById(productId);
+      if (this.product) {
+        this.otherProducts = await this.productService.getProductsByUserId(this.product.userId);
+        this.otherProducts = this.otherProducts.filter(p => p.id !== this.product!.id);
+      }
     }
   }
 
@@ -46,5 +56,10 @@ export class ProductDetailComponent implements OnInit {
       return this.product.userId === currentUserId;
     }
     return false;
+  }
+
+  async viewProduct(productId: string) {
+    await this.router.navigate(['/product', productId]);
+    await this.loadProductDetails();
   }
 }
